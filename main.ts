cb18ce805f3de1d575e494591282fa95b2594e62
Lucas Prieto <lucasprieto12@gmail.com>
2021-07-19 01:35:46 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+namespace SpriteKind {
+    export const Entity = SpriteKind.create()
+}
+let entities: Sprite = null
+game.splash("asdasd")
+tiles.placeOnRandomTile(entities, assets.tile`transparency16`)
