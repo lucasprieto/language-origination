185ee11ab14e630a727d19d0ef3409c8545ac352
Lucas Prieto <lucasprieto12@gmail.com>
2021-07-19 04:30:00 -0300

    Basic changes

--- main.ts
+++ main.ts
@@ -1,6 +1,20 @@
 namespace SpriteKind {
     export const Entity = SpriteKind.create()
 }
-let entities: Sprite = null
-game.splash("asdasd")
-tiles.placeOnRandomTile(entities, assets.tile`transparency16`)
+sprites.onCreated(SpriteKind.Entity, function (sprite) {
+    initEntity(sprite)
+    while (true) {
+    	
+    }
+})
+function isAlive (sprite: Sprite) {
+    return sprites.readDataNumber(sprite, "health") > 0
+}
+function initEntity (sprite: Sprite) {
+    sprites.setDataNumber(sprite, "maxHealth", 100)
+    sprites.setDataNumber(sprite, "health", 100)
+    sprites.setDataNumber(sprite, "age", 0)
+}
+function doEntityStep (sprite: Sprite) {
+	
+}
